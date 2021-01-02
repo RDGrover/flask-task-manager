7db964efe1f85d154870353692b6d8fdc57bd1ce
RDGrover <rossgrover95@hotmail.co.uk>
2021-01-02 14:29:38 +0000

    add task collapsible setup

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,6 @@
-$(document).ready(function(){
-    $(".sidenav").sidenav({edge: 'right'});
-});
+document.addEventListener('DOMContentLoaded', function () {
+    let sidenavs = document.querySelectorAll(".sidenav");
+    let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
+    let collapsibles = document.querySelectorAll(".collapsible");
+    let collapsiblesInstance = M.Collapsible.init(collapsibles);
+ });
